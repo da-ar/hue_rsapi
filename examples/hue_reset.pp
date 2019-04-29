4416cbf64dd7fc475d027958d15a980b954cabba
Dave Armstrong <david.armstrong@puppet.com>
2019-04-29 11:27:04 +0100

    Include Hue Emulator and additional config

--- examples/hue_reset.pp
+++ examples/hue_reset.pp
@@ -1,14 +1,23 @@
 hue_light {
-  '1':
-    on     => true,
-    bri    => 500,
-    hue    => 3000,
-    sat    => 255,
-    effect => 'none';
-  '2':
-    on     => true,
-    bri    => 500,
-    hue    => 3000,
-    sat    => 255,
-    effect => 'none';
+'1':
+  on     => true,
+  bri    => 254,
+  hue    => 4444,
+  sat    => 254,
+  effect => 'none',
+  alert  => 'none';
+'2':
+  on     => true,
+  bri    => 254,
+  hue    => 23536,
+  sat    => 144,
+  effect => 'none',
+  alert  => 'none';
+'3':
+  on     => true,
+  bri    => 254,
+  hue    => 65136,
+  sat    => 254,
+  effect => 'none',
+  alert  => 'none';
 }
